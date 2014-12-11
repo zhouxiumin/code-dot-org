b9b25d5364d56421f657ab9d2f486910d7f967be
Brian Jordan <bcjordan@gmail.com>
2014-12-11 10:39:21 -0800

    Add flappy notes

--- dashboard/public/offline/assets/notes/download_video_notes.rb
+++ dashboard/public/offline/assets/notes/download_video_notes.rb
@@ -1,7 +1,8 @@
 require 'open-uri'
-video_keys = %w(maze_intro loop_times loop_until if if_else_scrat)
+# video_keys = %w(maze_intro loop_times loop_until if if_else_scrat)
+video_keys = %w(flappy_intro)
 video_keys.each do |key|
   open("#{key}.html", 'wb') do |file|
-    file << open("http://0.0.0.0:3000/notes/#{key}").read
+    file << open("http://test.learn.code.org/notes/#{key}").read
   end
 end
