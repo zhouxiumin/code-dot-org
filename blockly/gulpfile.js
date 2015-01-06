bbfe879e853ca2d0dfea3171759a3b14e55af8c4
Will Jordan <will@code.org>
2015-01-05 16:03:17 -0800

    remove blockly-core compilation, and some refactoring

--- blockly/gulpfile.js
+++ blockly/gulpfile.js
@@ -1,24 +1,14 @@
 var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var transform = require('vinyl-transform');
-var watchify = require('watchify');
-var browserify = require('browserify');
-var exec = require('child_process').exec;
 var path = require('path');
 var es = require('event-stream');
 
-// Gulp and plugins
+// Gulp and general plugins
 var gulp = require('gulp');
 var newer = require('gulp-newer');
 var uglify = require('gulp-uglify');
 var gutil = require('gulp-util');
-var sourcemaps = require('gulp-sourcemaps');
 var insert = require('gulp-insert');
-var ejs = require('gulp-ejs-precompiler');
 var rename = require("gulp-rename");
-var messageFormat = require('gulp-messageformat');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
 
 var LOCALES = [
   'ar_sa',
@@ -88,61 +78,60 @@ var allFilesDest = [];
 var outputDir = './build/package/js/';
 APPS.forEach(function (app) {
   allFilesSrc.push('./build/js/' + app + '/main.js');
-  allFilesDest.push(outputDir+app+'.js');
+  allFilesDest.push(outputDir + app + '.js');
 });
 
+var browserify = require('browserify');
 var b = browserify(allFilesSrc);
-b.plugin('factor-bundle', { outputs: allFilesDest });
-gulp.task('browserify', ['src', 'vendor', 'ejs', 'messages'], function(){return bundle(b)});
-
-gulp.task('compress', ['browserify'],function(){
-  return gulp.src('./build/package/js/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('./build/package/js'))
+b.plugin('factor-bundle', {outputs: allFilesDest});
+gulp.task('browserify', ['src', 'vendor', 'ejs', 'messages'], function () {
+  return bundle(b);
 });
 
+var watchify = require('watchify');
 var w = watchify(browserify(allFilesSrc, watchify.args));
-w.plugin('factor-bundle', { outputs: allFilesDest });
-w.on('update', function(){gutil.log('Updated'); bundle(w)}); // on any dep update, runs the bundler
-gulp.task('watch', ['src', 'vendor', 'ejs', 'messages', 'media', 'sass'], function(){
-  gulp.watch('src/**/*.js', ['src']);
-  gulp.watch('src/**/*.ejs', ['ejs']);
-  bundle(w);
-});
+w.plugin('factor-bundle', {outputs: allFilesDest});
+w.on('update', function () {
+  gutil.log('Updated');
+  bundle(w)
+}); // on any dep update, runs the bundler
 
 function bundle(bundler) {
   return bundler.bundle()
     // log errors if they happen
     .on('error', gutil.log.bind(gutil, 'Browserify Error'))
     .pipe(source('common.js'))
-    // optional, remove if you dont want sourcemaps
-    .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
-    .pipe(sourcemaps.write('./')) // writes .map file
-    //
     .pipe(gulp.dest('./build/package/js'));
 }
 
-gulp.task('messages', function(){
+gulp.task('compress', ['browserify'], function () {
+  return gulp.src(['./build/package/js/*.js', './build/package/js/**/en_us/*.js', '!build/package/js/**/vendor.js'])
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/package/js'))
+});
+
+var messageFormat = require('gulp-messageformat');
+gulp.task('messages', function () {
   var messageStreams = [];
   LOCALES.forEach(function (locale) {
     var language = locale.split('_')[0];
     APPS.concat('common').forEach(function (app) {
       var namespace = (app == 'common' ? 'locale' : 'appLocale');
       messageStreams.push(
-        gulp.src('i18n/'+app+'/'+locale+'.json')
-        .pipe(newer('./build/package/js/'+locale+'/'+app+'_locale.js'))
-        .pipe(messageFormat({locale:language, namespace:namespace + 'Blockly'}))
-        .pipe(rename(app+'_locale.js'))
-        .pipe(insert.wrap("window.blockly = window.blockly || {};\n","\nwindow.blockly."+namespace + " = " + namespace + "Blockly['"+locale+"'];\n"))
-        .pipe(gulp.dest('build/package/js/'+locale))
+        gulp.src('i18n/' + app + '/' + locale + '.json')
+          .pipe(newer('./build/package/js/' + locale + '/' + app + '_locale.js'))
+          .pipe(messageFormat({locale: language, namespace: namespace + 'Blockly'}))
+          .pipe(rename(app + '_locale.js'))
+          .pipe(insert.wrap("window.blockly = window.blockly || {};\n", "\nwindow.blockly." + namespace + " = " + namespace + "Blockly['" + locale + "'];\n"))
+          .pipe(gulp.dest('build/package/js/' + locale))
       );
     });
   });
   return es['merge'].apply(this, messageStreams);
 });
 
-gulp.task('ejs', function(){
+var ejs = require('gulp-ejs-precompiler');
+gulp.task('ejs', function () {
   return gulp.src('./src/**/*.ejs')
     .pipe(ejs({
       client: true,
@@ -150,61 +139,58 @@ gulp.task('ejs', function(){
       compileDebug: false
     }))
     .pipe(insert.wrap('module.exports = function(a,b,c,d) {\ntemplates = {};\n',
-      function(file) {
+      function (file) {
         var templatePath = JSON.stringify(file.relative.slice(0, -3));
         return '\n' + 'return templates[' + templatePath + '](a,b,c,d);\n}'
       }))
     .pipe(gulp.dest('./build/js'));
 });
 
-  var ext = 'uncompressed';
-var vendorFilesNew = [
-  './build/blockly.js',
-  './build/blocks.js',
-  './build/javascript.js',
-    'lib/blockly/' + 'en_us' + '.js'
+var ext = 'compressed';
+var vendorFiles = [
+  'lib/blockly/blockly_' + ext + '.js',
+  'lib/blockly/blocks_' + ext + '.js',
+  'lib/blockly/javascript_' + ext + '.js',
+  'lib/blockly/' + 'en_us' + '.js'
 ];
 
-  var vendorFiles = [
-      'lib/blockly/blockly_' + ext + '.js',
-      'lib/blockly/blocks_' + ext + '.js',
-      'lib/blockly/javascript_' + ext + '.js',
-      'lib/blockly/' + 'en_us' + '.js'
-  ];
-
-gulp.task('vendor', /*['core'],*/ function() {
+var concat = require('gulp-concat');
+gulp.task('vendor', function () {
   return gulp.src(vendorFiles)
     .pipe(newer('./build/package/js/en_us/vendor.js'))
-    .pipe(uglify())
     .pipe(concat('vendor.js'))
     .pipe(gulp.dest('./build/package/js/en_us'))
 });
 
-gulp.task('media', function() {
+gulp.task('media', function () {
   return gulp.src(['static/**/*', 'lib/blockly/media/**/*'])
+    .pipe(newer('./build/package/media'))
     .pipe(gulp.dest('./build/package/media'))
 });
 
-gulp.task('src', function() {
+gulp.task('src', function () {
   return gulp.src(['src/**/*.js'])
+    .pipe(newer('./build/js'))
     .pipe(gulp.dest('./build/js'))
 });
 
-gulp.task('lodash', function(cb){
+var exec = require('child_process').exec;
+gulp.task('lodash', function (cb) {
   return exec('`npm bin`/lodash include=' +
-    ['debounce', 'reject', 'map', 'value', 'range', 'without', 'sample',
+  ['debounce', 'reject', 'map', 'value', 'range', 'without', 'sample',
     'create', 'flatten', 'isEmpty', 'wrap']
-      .join(',') + ' --output src/lodash.js', cb);
+    .join(',') + ' --output src/lodash.js', cb);
 });
 
+var sass = require('gulp-sass');
 gulp.task('sass', function () {
   return es.merge(
     gulp.src('style/common.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./build/package/css')),
+      .pipe(sass())
+      .pipe(gulp.dest('./build/package/css')),
     gulp.src('style/**/style.scss')
       .pipe(sass())
-      .pipe(rename(function(filepath) {
+      .pipe(rename(function (filepath) {
         // replace file name to that of the parent directory
         filepath.basename = path.basename(filepath.dirname);
         // remove parent directory from relative path
@@ -215,51 +201,15 @@ gulp.task('sass', function () {
   );
 });
 
-gulp.task('build', ['compress', 'media', 'sass']);
+gulp.task('build', ['browserify', 'media', 'sass']);
+// Call 'package' for maximum compression of all .js files
+gulp.task('package', ['compress', 'media', 'sass']);
 
-var closureCompiler = require('gulp-closure-compiler');
-var closure = function(filename, blocklyMain) {
-  return closureCompiler({
-    fileName: filename,
-    compilerPath: '../blockly-core/compiler.jar',
-    compilerFlags: blocklyMain ? {
-      closure_entry_point: 'Blockly',
-      only_closure_dependencies: true,
-      compilation_level: 'WHITESPACE_ONLY'
-    } : {
-      compilation_level: 'WHITESPACE_ONLY'
-    }
-  })
-};
-
-gulp.task('core', ['core-blockly', 'core-javascript', 'core-blocks']);
-
-gulp.task('core-blockly', function() {
-  return gulp.src([
-    '../blockly-core/core/**/*.js',
-    '../blockly-core/closure-library-read-only/**/*.js'
-  ])
-    .pipe(newer('./build/blockly.js'))
-    .pipe(closure('blockly.js', true))
-    .pipe(gulp.dest('./build'));
-});
-
-gulp.task('core-javascript', function() {
-  return gulp.src([
-    '../blockly-core/generators/**/*.js',
-    '!../blockly-core/generators/**/*python*.js',
-    '!../blockly-core/generators/**/python/*.js'
-  ])
-    .pipe(newer('./build/javascript.js'))
-    .pipe(closure('javascript.js'))
-    .pipe(gulp.dest('./build'));
-});
-
-gulp.task('core-blocks', function() {
-  return gulp.src([
-    '../blockly-core/blocks/*.js'
-  ])
-    .pipe(newer('./build/blocks.js'))
-    .pipe(closure('blocks.js'))
-    .pipe(gulp.dest('./build'));
+gulp.task('dev', ['src', 'vendor', 'ejs', 'messages', 'media', 'sass'], function () {
+  gulp.watch('src/**/*.js', ['src']);
+  gulp.watch('src/**/*.ejs', ['ejs']);
+  gulp.watch('i18n/**/*.json', ['messages']);
+  gulp.watch(['static/**/*', 'lib/blockly/media/**/*'], ['media']);
+  gulp.watch('style/**/*.scss', ['sass']);
+  bundle(w);
 });
