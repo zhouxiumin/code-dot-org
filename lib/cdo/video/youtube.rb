73b9a1acc00a1611faca959f70a139482a19d0ab
Will Jordan <will@code.org>
2015-07-16 18:32:34 -0700

    update youtube

--- lib/cdo/video/youtube.rb
+++ lib/cdo/video/youtube.rb
@@ -16,18 +16,17 @@ class Youtube
     url = "www.youtube.com/watch?v=#{id}"
 
     Dir.mktmpdir do |dir|
-
-      puts `viddl-rb #{url} -s #{dir} -q 640:360:mp4`
+      # Run viddl-rb, printing output
+      cmd = "viddl-rb #{url} -s #{dir} -q 640:360:mp4"
+      IO.popen(cmd) { |output| output.each { |line| puts line } }
       file = Dir.glob("#{dir}/*").first
-      if File.extname(file) != '.mp4'
-        puts 'Error: video not available in correct format'
-      else
-        filename = AWS::S3.upload_to_bucket(VIDEO_BUCKET, "youtube/#{id}.mp4", File.open(file), access: :public_read, no_random: true, content_type: 'video/mp4')
-        puts "https://#{VIDEO_BUCKET}/#{filename}"
-        thumbnail_file = "https://i.ytimg.com/vi/#{id}/0.jpg"
-        thumbnail_filename = AWS::S3.upload_to_bucket(VIDEO_BUCKET, "youtube/#{id}.jpg", open(thumbnail_file), access: :public_read, no_random: true)
-        puts "https://#{VIDEO_BUCKET}/#{thumbnail_filename}"
-      end
+      raise RuntimeError, 'Video not available in correct format' if File.extname(file) != '.mp4'
+      video_filename = AWS::S3.upload_to_bucket(VIDEO_BUCKET, "youtube/#{id}.mp4", File.open(file), access: :public_read, no_random: true, content_type: 'video/mp4')
+      puts "https://#{VIDEO_BUCKET}/#{video_filename}"
+      thumbnail_file = "https://i.ytimg.com/vi/#{id}/0.jpg"
+      thumbnail = open(thumbnail_file) || raise RuntimeError, 'Could not retrieve thumbnail for video'
+      thumbnail_filename = AWS::S3.upload_to_bucket(VIDEO_BUCKET, "youtube/#{id}.jpg", thumbnail, access: :public_read, no_random: true)
+      puts "https://#{VIDEO_BUCKET}/#{thumbnail_filename}"
     end
   end
 end
