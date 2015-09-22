4658e48463bcc4f7b8f3dd2802f8e19340e0817f
Will Jordan <will@code.org>
2015-09-22 13:11:13 -0700

    update cdo-ruby-2.2

--- cookbooks/cdo-ruby-2.2/attributes/default.rb
+++ cookbooks/cdo-ruby-2.2/attributes/default.rb
@@ -1,4 +1,4 @@
 default['brightbox-ruby']['version'] = '2.2'
 default['brightbox-ruby']['rubygems_version'] = '2.4.8'
-default['brightbox-ruby']['gems'] = %w(bundler rake)
+default['brightbox-ruby']['gems'] = %w(bundler rake) # Workaround for mojolingo/brightbox-ruby-cookbook#5
 default['cdo-ruby-2.2']['bundler_version'] = '1.10.6'
--- cookbooks/cdo-ruby-2.2/recipes/default.rb
+++ cookbooks/cdo-ruby-2.2/recipes/default.rb
@@ -1,17 +1,13 @@
+# Workaround for mojolingo/brightbox-ruby-cookbook#5
 gem_package 'rubygems-bundler' do
   action :nothing
 end
-
 include_recipe 'brightbox-ruby'
+# Ensure proper symlinks are set
 %w(ruby gem).each{|alt| execute "update-alternatives --force --auto #{alt}" }
 
+# Upgrade Bundler
 gem_package 'bundler' do
   action :upgrade
   version node['cdo-ruby-2.2']['bundler_version']
-end
-
-execute 'gem update --system' do
-  command "gem update -q --system '#{node['brightbox-ruby']['rubygems_version']}'"
-  environment 'REALLY_GEM_UPDATE_SYSTEM' => '1'
-  not_if "which gem > /dev/null && gem --version | grep -q '#{node['brightbox-ruby']['rubygems_version']}'"
 end
