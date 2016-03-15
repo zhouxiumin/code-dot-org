b78cd75637effd27ee90b88d6cc964b0de68c803
Will Jordan <will@code.org>
2016-03-15 13:12:20 -0700

    working VPC stack [ci skip]

--- aws/full_stack_adhoc.sh
+++ aws/full_stack_adhoc.sh
@@ -20,6 +20,6 @@ STACK_FILE=chef-stack.yml.erb
 # us-west-2 ubuntu AMI
 export IMAGE_ID=ami-9abea4fb
 
-# Process packer config with ERB + YAML, then pass the JSON to aws cloudformation.
-aws cloudformation create-stack --stack-name adhoc-frontend-${BRANCH}-2 --region us-west-2 --template-body \
+# Process CloudFormation config with ERB + YAML, then pass the JSON to `aws cloudformation create-stack`.
+aws cloudformation create-stack --stack-name adhoc-frontend-${BRANCH}2 --region us-west-2 --template-body \
   "$(ruby -rerb -ryaml -rjson -e "puts YAML.load(ERB.new(File.read('${STACK_FILE}')).result).to_json")"
