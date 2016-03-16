8a7623a6ed6cc41c194191d2d0749a4379c7d78d
Will Jordan <will@code.org>
2016-03-15 17:07:06 -0700

    signal stack, rolling updates [ci skip]

--- aws/full_stack_adhoc.sh
+++ aws/full_stack_adhoc.sh
@@ -21,5 +21,5 @@ STACK_FILE=chef-stack.yml.erb
 export IMAGE_ID=ami-9abea4fb
 
 # Process CloudFormation config with ERB + YAML, then pass the JSON to `aws cloudformation create-stack`.
-aws cloudformation create-stack --stack-name adhoc-frontend-${BRANCH}2 --region us-west-2 --template-body \
+aws cloudformation update-stack --stack-name adhoc-frontend-${BRANCH}2 --region us-west-2 --template-body \
   "$(ruby -rerb -ryaml -rjson -e "puts YAML.load(ERB.new(File.read('${STACK_FILE}')).result).to_json")"
