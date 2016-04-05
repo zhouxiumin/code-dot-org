30a1aa4e5b5438456ab93018b47ca710df0aea65
Will Jordan <will@code.org>
2016-04-05 05:08:37 +0000

    update solr integration test

--- cookbooks/cdo-solr/test/integration/default/serverspec/ruby_spec.rb
+++ cookbooks/cdo-solr/test/integration/default/serverspec/ruby_spec.rb
@@ -1,17 +1,15 @@
 require 'serverspec'
 set :backend, :exec
 
-def file_exist(file)
-  describe file(file) do
-    it { should exist }
-  end
-end
-
 def cmd(exec, match)
   describe command(exec) do
     its(:stdout) { should match match }
   end
 end
 
-file_exist 'solr'
-cmd 'solr -v', 'solr'
+describe service('solr') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+cmd '/opt/solr/bin/solr -version', '5.5.0'
