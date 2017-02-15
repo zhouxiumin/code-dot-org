a97a5d656bfc1febb11b757032aed194674c754a
Josh Lory <josh.lory@code.org>
2017-02-15 11:57:32 -0800

    Move `with_difficluty` logic down to a scope in the Arena model

--- dashboard/app/controllers/arena_controller.rb
+++ dashboard/app/controllers/arena_controller.rb
@@ -24,21 +24,14 @@ class ArenaController < ApplicationController
       if arena_params[:level_id]
         Level.cache_find(arena_params[:level_id])
       elsif arena_params[:concept]
-        cached_level_for_concpet(arena_params[:concept])
+        cached_level_for_concept(arena_params[:concept].to_sym)
       end
     @game = @level.game
   end
 
-  def cached_level_for_concpet(concept)
-    if @@level_concept_cache[concept].nil?
-      # Exclude levels that are tagged with other concepts.
-      # Most levels are also tagged 'sequencing', don't exclude these.
-      others = LevelConceptDifficulty::CONCEPTS - [concept, 'sequencing']
-      others_query = others.map{ |s| [s, nil] }.to_h
-
-      # Find all non-trivial levels (concept difficulty 2-5).
-      @@level_concept_cache[concept] = LevelConceptDifficulty.where(concept => (2..5)).where(others_query).pluck(:level_id)
-    end
+  def cached_level_for_concept(concept)
+    # Find all non-trivial levels (concept difficulty 2-5).
+    @@level_concept_cache[concept] ||= LevelConceptDifficulty.with_difficulty(concept => (2..5)).pluck(:level_id)
 
     # Pick one level at random.
     Level.cache_find(@@level_concept_cache[concept].sample)
--- dashboard/app/models/level_concept_difficulty.rb
+++ dashboard/app/models/level_concept_difficulty.rb
@@ -27,6 +27,19 @@ class LevelConceptDifficulty < ActiveRecord::Base
   belongs_to :level
   validates :level, presence: true
 
+  # Find levels only tagged with specific concept difficulties. Any unspecified
+  # concept will default to `nil` (except 'sequencing', since most levels are
+  # tagged with this concept).
+  #
+  # For example, `with_difficulty(repeat_loops: 2..5)` will find all non-trivial
+  # loop LevelConceptDifficulties (difficulty 2-5), excluding any that are also
+  # tagged with other concepts (but allowing 'sequencing').
+  scope :with_difficulty, ->(concepts) do
+    others = LevelConceptDifficulty::CONCEPTS.map(&:to_sym) - [:sequencing] - concepts.keys
+    others_query = others.map{ |s| [s, nil] }.to_h
+    LevelConceptDifficulty.where(concepts).where(others_query)
+  end
+
   def serializable_hash(options=nil)
     super.compact.delete_if do |key, _|
       %w(id level_id updated_at created_at).include? key
