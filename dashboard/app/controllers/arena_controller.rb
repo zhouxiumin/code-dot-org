747e8d5061b28fb834e39eb7962f90ba02f5ee9b
Josh Lory <josh.lory@code.org>
2017-02-15 09:40:53 -0800

    Allow choosing levels in the arena by concept or ID
    When choosing by concept, find a level that has non-trivial difficulty (2-5) for the given concept, and isn't tagged with any other concept (except possibly 'sequencing').

--- dashboard/app/controllers/arena_controller.rb
+++ dashboard/app/controllers/arena_controller.rb
@@ -1,7 +1,9 @@
 class ArenaController < ApplicationController
   before_action :set_level, only: [:show]
 
-  # GET /arenas
+  @@level_concept_cache = {}
+
+  # GET /arena
   def show
     @never_autoplay_video = true
 
@@ -18,12 +20,32 @@ class ArenaController < ApplicationController
   private
 
   def set_level
-    @level = Level.cache_find(arena_params)
+    @level =
+      if arena_params[:level_id]
+        Level.cache_find(arena_params[:level_id])
+      elsif arena_params[:concept]
+        cached_level_for_concpet(arena_params[:concept])
+      end
     @game = @level.game
+  end
+
+  def cached_level_for_concpet(concept)
+    if @@level_concept_cache[concept].nil?
+      # Exclude levels that are tagged with other concepts.
+      # Most levels are also tagged 'sequencing', don't exclude these.
+      others = LevelConceptDifficulty::CONCEPTS - [concept, 'sequencing']
+      others_query = others.map{ |s| [s, nil] }.to_h
+
+      # Find all non-trivial levels (concept difficulty 2-5).
+      @@level_concept_cache[concept] = LevelConceptDifficulty.where(concept => (2..5)).where(others_query).pluck(:level_id)
+    end
+
+    # Pick one level at random.
+    Level.cache_find(@@level_concept_cache[concept].sample)
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def arena_params
-    params.require(:level_id)
+    params.permit(:level_id, :concept)
   end
 end
