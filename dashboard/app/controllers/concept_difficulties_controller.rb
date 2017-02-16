ead2f1bc9597e564ac66d7d5f264e1f3a9c01370
Josh Lory <josh.lory@code.org>
2017-02-16 09:46:10 -0800

    Tranpose the concept matrix

--- dashboard/app/controllers/concept_difficulties_controller.rb
+++ dashboard/app/controllers/concept_difficulties_controller.rb
@@ -4,7 +4,7 @@ class ConceptDifficultiesController < ApplicationController
     @concepts = LevelConceptDifficulty::CONCEPTS
     @matrix = @concepts.map do |concept_a|
       @concepts.map do |concept_b|
-        LevelConceptDifficulty.relative_incidence(concept_a, concept_b).first
+        LevelConceptDifficulty.relative_incidence(concept_a, concept_b).last
       end
     end
   end
