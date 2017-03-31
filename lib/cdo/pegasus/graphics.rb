51ac5ba82f560700348fc307a2a90918a377ae4a
Will Jordan <will@code.org>
2017-03-31 14:29:52 -0700

    refactor and add comments to `#load_manipulated_image`

--- lib/cdo/pegasus/graphics.rb
+++ lib/cdo/pegasus/graphics.rb
@@ -2,9 +2,15 @@ require 'rmagick'
 
 MAX_DIMENSION = 2880
 
-# This method returns a newly-allocated Magick::Image object.
-# NOTE: the caller MUST ensure image#destroy! is called on the returned image object to avoid memory leaks.
-def load_manipulated_image(path, mode, width, height, scale = nil)
+# Load an image file and return manipulated image contents.
+# @param path [String] absolute path to image file
+# @param mode [Symbol] operation mode (:fill, :fit or :resize)
+# @param width [Integer] target image width
+# @param height [Integer] target image height
+# @param format [String] output image format
+# @param scale [Float] scale width/height by factor
+# @return [String] Manipulated image data blob
+def load_manipulated_image(path, mode, width, height, format, scale = nil)
   image = Magick::Image.read(path).first
 
   # If only one dimension provided, assume a square
@@ -33,58 +39,66 @@ def load_manipulated_image(path, mode, width, height, scale = nil)
     else
       nil
   end
+
+  image.format = format
+  image.to_blob
+ensure
+  image && image.destroy!
 end
 
 def process_image(path, ext_names, language=nil, site=nil)
-  extname = File.extname(path).downcase
-  return nil unless ext_names.include?(extname)
-  image_format = extname[1..-1]
+  path_ext = File.extname(path).downcase
+  return nil unless ext_names.include?(path_ext)
+  image_format = path_ext[1..-1]
 
-  basename = File.basename(path, extname)
-  dirname = File.dirname(path)
+  path_base = File.basename(path, path_ext)
+  path_dir = File.dirname(path)
 
   mode = :resize
   width = nil
   height = nil
   manipulated = false
 
-  # Manipulated?
-  if (m = dirname.match /^(?<basedir>.*)\/(?<mode>fit-|fill-)?(?<width>\d*)x?(?<height>\d*)(\/(?<dir>.*))?$/m)
+  # Extract image path and manipulation options from request path info.
+  if (m = path_dir.match /^(?<basedir>.*)\/(?<mode>fit-|fill-)?(?<width>\d*)x?(?<height>\d*)(\/(?<dir>.*))?$/m)
     mode = m[:mode][0..-2].to_sym unless m[:mode].nil_or_empty?
     width = m[:width].to_i unless m[:width].nil_or_empty?
     height = m[:height].to_i unless m[:height].nil_or_empty?
     manipulated = width || height
-    dirname = File.join(m[:basedir].to_s, m[:dir].to_s)
+    path_dir = File.join(m[:basedir].to_s, m[:dir].to_s)
   end
 
   # Assume we are returning the same resolution as we're reading.
-  retina_in = retina_out = basename[-3..-1] == '_2x'
+  retina_in = retina_out = path_base[-3..-1] == '_2x'
 
-  path = nil
+  image_file = nil
   if site == 'hourofcode.com'
-    path = resolve_image File.join(language, dirname, basename)
+    image_file = resolve_image File.join(language, path_dir, path_base)
   end
-  path ||= resolve_image File.join(dirname, basename)
-  unless path
+  image_file ||= resolve_image File.join(path_dir, path_base)
+  unless image_file
     # Didn't find a match at this resolution, look for a match at the other resolution.
     if retina_out
-      basename = basename[0...-3]
+      path_base = path_base[0...-3]
       retina_in = false
     else
-      basename += '_2x'
+      path_base += '_2x'
       retina_in = true
     end
-    path = resolve_image File.join(dirname, basename)
+    image_file = resolve_image File.join(path_dir, path_base)
   end
-  return nil unless path # No match at any resolution.
+
+  return nil unless image_file # No match at any resolution.
+
+  # Found a valid image file based on the path info.
   output = {
-    last_modified: File.mtime(path),
+    last_modified: File.mtime(image_file),
     content_type: image_format.to_sym,
   }
 
-  if (retina_out || !retina_in) && !manipulated && File.extname(path) == extname
+  if (retina_out || !retina_in) && !manipulated && File.extname(image_file) == path_ext
     # No [useful] modifications to make, return the original.
-    return output.merge(file: path)
+    return output.merge(file: image_file)
   end
 
   scale = 1
@@ -92,15 +106,10 @@ def process_image(path, ext_names, language=nil, site=nil)
     # Manipulated images always specify non-retina sizes in the manipulation string.
     scale = 2 if retina_out
   else
-    # Retina sources need to be downsampled for non-retina output
+    # Retina sources need to be downsampled for non-retina output.
     scale = 0.5 if retina_in && !retina_out
   end
 
-  begin
-    image = load_manipulated_image(path, mode, width, height, scale)
-    image.format = image_format
-    output.merge(content: image.to_blob)
-  ensure
-    image && image.destroy!
-  end
+  image = load_manipulated_image(image_file, mode, width, height, image_format, scale)
+  output.merge(content: image)
 end
