a68f642a6822319412259ba33524a538df326ecf
Will Jordan <will@code.org>
2017-04-04 17:59:12 -0700

    Use shared S3 cache for Sprockets precompiled assets

--- dashboard/lib/tasks/asset_sync.rake
+++ dashboard/lib/tasks/asset_sync.rake
@@ -6,8 +6,19 @@ namespace :assets do
     cmd = "aws s3 sync #{dashboard_dir}/public/assets s3://#{CDO.assets_bucket}/#{rack_env}/assets --acl public-read --cache-control 'max-age=31536000'"
     RakeUtils.system cmd
   end
+
+  task pre_precompile: :environment do
+    next unless CDO.sync_assets
+    require 'cdo/aws/sprockets_cache_s3store'
+    Rails.application.config.assets.configure do |env|
+      env.cache = Sprockets::Cache::S3Store.new(CDO.assets_bucket, "#{rack_env}/sprockets_cache.gz")
+    end
+  end
 end
 
-Rake::Task['assets:precompile'].enhance do
+Rake::Task['assets:precompile'].enhance([:pre_precompile]) do
+  next unless CDO.sync_assets
+  cache = Rails.application.assets.cache.instance_variable_get(:@cache_wrapper).cache
+  cache.flush if cache.respond_to?(:flush)
   Rake::Task['assets:sync'].invoke if CDO.sync_assets
 end
---
+++ lib/cdo/aws/sprockets_cache_s3store.rb
@@ -0,0 +1,82 @@
+require 'aws-sdk'
+require 'sprockets/cache'
+require 'sprockets/encoding_utils'
+
+module Sprockets
+  class Cache
+    # Basic Sprockets cache store over S3.
+    #
+    # Individual S3 requests are too high-latency for each cache entry to use its own S3 object.
+    # Instead, load the entire cache from a single S3 object at startup, and flush the cache back to S3 after
+    # all Sprockets processing has completed.
+    #
+    # Assign the instance to the Environment#cache, e.g.:
+    #
+    #     environment.cache = Sprockets::Cache::S3Store.new('my-s3bucket', 'sprockets-assets')
+    class S3Store < MemoryStore
+
+      attr_reader :bucket, :key, :s3, :logger
+
+      # Initialize the cache store.
+      #
+      # @param [String] bucket S3 bucket name
+      # @param [String] key S3 object key prefix
+      def initialize(bucket, key = 'sprockets')
+        super(100000)
+        @s3 = Aws::S3::Client.new
+        @bucket = bucket
+        @key = key
+        @logger = Logger.new(STDOUT)
+        @logger.level = Logger::INFO
+      end
+
+      # Write the Sprockets cache back to S3 object if dirty.
+      def flush
+        return unless @dirty
+        logger.info 'Flushing cache..'
+        s3.put_object(
+          bucket: bucket,
+          key: key,
+          body: EncodingUtils.deflate(Marshal.dump(@cache))
+        )
+        logger.info 'Flushed cache to S3'
+        @dirty = false
+      end
+
+      def get(key)
+        unless @cache_loaded
+          @cache_loaded = true
+          load_cache
+        end
+        super(key)
+      end
+
+      def set(key, value)
+        unless @cache[key] === value
+          @dirty = true
+          logger.debug "Value changed: #{key}"
+        end
+        super(key, value)
+      end
+
+      private
+
+      # Load the Sprockets cache from S3 object, if available.
+      def load_cache
+        logger.info 'Loading Sprockets cache from S3..'
+        @dirty = false
+        @cache = EncodingUtils.unmarshalled_deflated(
+          s3.get_object(bucket: bucket, key: key).
+            body.read.force_encoding(Encoding::BINARY)
+        )
+        logger.info "Loaded cache: #{self.inspect}"
+      rescue Aws::S3::Errors::NoSuchKey
+        logger.info 'Cache not found'
+        # ignored
+      rescue Exception => e
+        logger.warn "#{self.class}[#{bucket}/#{key}] could not be unmarshaled: " +
+          "#{e.class}: #{e.message}"
+      end
+    end
+  end
+end
