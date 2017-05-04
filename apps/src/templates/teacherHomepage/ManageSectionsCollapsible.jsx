2761172d7ef3bf57e1384746b76b68a1f25eb33c
Erin Bond <erin.bond@code.org>
2017-05-04 10:55:34 -0700

    Use SectionsTable in ManageSectionsCollapsible

--- apps/src/templates/teacherHomepage/ManageSectionsCollapsible.jsx
+++ apps/src/templates/teacherHomepage/ManageSectionsCollapsible.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import CollapsibleSection from './CollapsibleSection';
-// import SectionsTable from './SectionsTable';
+import SectionsTable from './SectionsTable';
 import SetUpMessage from './SetUpMessage';
 import i18n from "@cdo/locale";
 
@@ -14,13 +14,15 @@ const ManageSectionsCollapsible = React.createClass({
 
     return (
       <CollapsibleSection
-        header={i18n.mySections()}
+        header={i18n.sectionTitle()}
         linkText={i18n.editSections()}
         link="https://code.org/teacher-dashboard#/sections"
       >
       {sections ? (
         <div>
-          This is where the sections table will go.
+          <SectionsTable
+            sections={sections}
+          />
         </div>
       ) : (
         <SetUpMessage type="sections"/>
