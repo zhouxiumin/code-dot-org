7734e2e16067f7b8689c26f85a3f7ee7274e42c7
Erin Bond <erin.bond@code.org>
2017-05-09 17:11:04 -0700

    Start to incorporate real data for announcements on the teacher homepage

--- apps/src/sites/studio/pages/home/teacher_homepage.js
+++ apps/src/sites/studio/pages/home/teacher_homepage.js
@@ -12,16 +12,13 @@ function showTeacherHomepage() {
   const sectionsData = document.querySelector('script[data-sections]');
   const configSections = JSON.parse(sectionsData.dataset.sections);
 
+  const announcementsData = document.querySelector('script[data-announcements]');
+  console.log(announcementsData);
+  const configAnnouncements = JSON.parse(announcementsData.dataset.announcements);
+
   ReactDOM.render (
     <TeacherHomepage
-      announcements={[
-        {
-          heading: "Go beyond an Hour of Code",
-          buttonText: "Go Beyond",
-          description: "Go Beyond an Hour of Code and explore computer science concepts with your students every week. Code.org offers curriculum, lesson plans, high quality professional learning programs, and tons of great tools for all grade levels - and it's free. No experience required - find the next step that's right for your classroom.",
-          link: "to wherever"
-        }
-      ]}
+      announcements={configAnnouncements}
       courses={configCourses}
       sections={configSections}
     />,
