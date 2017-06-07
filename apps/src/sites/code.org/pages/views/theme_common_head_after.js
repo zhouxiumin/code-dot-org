5b8f8b194825b197fd6e702b339bfacd40bec264
Will Jordan <will@code.org>
2017-06-07 08:30:33 -0700

    move webpack bundle from head after to body after shift load-order to prioritize DOM content before JS enhancements.

--- apps/src/sites/code.org/pages/views/theme_common_head_after.js
+++ apps/src/sites/code.org/pages/views/theme_common_head_after.js
@@ -1,13 +1,20 @@
+// This Webpack-bundled content runs after DOMContentLoaded.
+// TODO rename to theme_common_body_after.js
+
 import 'details-element-polyfill';
 
 window.lazySizesConfig = window.lazySizesConfig || {};
 window.lazySizesConfig.init = false;
 import * as lazySizes from 'lazysizes';
 import 'lazysizes/plugins/unveilhooks/ls.unveilhooks';
+document.addEventListener('DOMContentLoaded', function () {
+  lazySizes.init();
+  if (isUnsupportedBrowser()) {
+    $("#warning-banner").show();
+  }
+});
 
 import {isUnsupportedBrowser} from '@cdo/apps/util/browser-detector';
-import {initHamburger} from '@cdo/apps/hamburger/hamburger';
-import {loadVideos} from '@cdo/apps/util/loadVideos';
 
 // Prevent filtered errors from being passed to New Relic.
  if (window.newrelic) {
@@ -17,12 +24,8 @@ import {loadVideos} from '@cdo/apps/util/loadVideos';
    });
  }
 
-document.addEventListener('DOMContentLoaded', function () {
-  lazySizes.init();
-  if (isUnsupportedBrowser()) {
-    $("#warning-banner").show();
-  }
-});
-
+import {initHamburger} from '@cdo/apps/hamburger/hamburger';
 initHamburger();
+
+import {loadVideos} from '@cdo/apps/util/loadVideos';
 window.loadVideos = loadVideos;
