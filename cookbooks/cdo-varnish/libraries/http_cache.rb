f167c90143401e764f649439e492c5faea999338
Will Jordan <will@code.org>
2017-06-29 10:55:19 -0700

    add code.org/assets/* to http-cache config Route asset_path to /assets avoiding cross-domain request. Update pegasus_sites to process http-cache for all proxy-routing behaviors.

--- cookbooks/cdo-varnish/libraries/http_cache.rb
+++ cookbooks/cdo-varnish/libraries/http_cache.rb
@@ -68,6 +68,14 @@ class HttpCache
       pegasus: {
         behaviors: [
           {
+            # Serve Sprockets-bundled assets directly from the S3 bucket synced via `assets:precompile`.
+            #
+            path: '/assets/*',
+            proxy: 'cdo-assets',
+            headers: [],
+            cookies: 'none'
+          },
+          {
             path: '/api/hour/*',
             headers: LANGUAGE_HEADER,
             # Allow the company cookie to be read and set to track company users for tutorials.
@@ -75,7 +83,7 @@ class HttpCache
           },
           # For static-asset paths, don't forward any cookies or additional headers.
           {
-            path: STATIC_ASSET_EXTENSION_PATHS + %w(/files/* /images/* /assets/* /fonts/*),
+            path: STATIC_ASSET_EXTENSION_PATHS + %w(/files/* /images/* /fonts/*),
             headers: [],
             cookies: 'none'
           },
