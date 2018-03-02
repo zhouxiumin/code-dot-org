a2e0a316dce51b82c55e811a8a2e3cfe85005750
Will Jordan <will@code.org>
2018-03-02 14:19:42 -0800

    updates to Discourse SSO integration - Sync `single_sign_on.rb` with upstream changes - Update DiscourseSsoController to redirect to provided `return_sso_url`. Simplifies configuration and allows multiple forum domains to use the same Code Studio domain for SSO authentication.

--- dashboard/lib/single_sign_on.rb
+++ dashboard/lib/single_sign_on.rb
@@ -7,16 +7,30 @@ class SingleSignOn
     :email,
     :avatar_url,
     :avatar_force_update,
-    :about_me,
+    :require_activation,
+    :bio,
     :external_id,
     :return_sso_url,
     :admin,
-    :moderator
+    :moderator,
+    :suppress_welcome_message,
+    :title,
+    :add_groups,
+    :remove_groups,
+    :groups
   ].freeze
-  BOOLS = [:avatar_force_update, :admin, :moderator].freeze
+  FIXNUMS = [].freeze
+  BOOLS = [
+    :avatar_force_update,
+    :admin,
+    :moderator,
+    :require_activation,
+    :suppress_welcome_message
+  ].freeze
+  ARRAYS = [:groups].freeze
 
   attr_accessor(*ACCESSORS)
-  attr_accessor :sso_secret, :sso_url
+  attr_writer :sso_secret, :sso_url
 
   def self.sso_secret
     raise "sso_secret not implemented on class, be sure to set it on instance"
@@ -45,23 +59,25 @@ class SingleSignOn
 
     ACCESSORS.each do |k|
       val = decoded_hash[k.to_s]
+      val = val.to_i if FIXNUMS.include? k
       if BOOLS.include? k
         val = ["true", "false"].include?(val) ? val == "true" : nil
       end
+      val = Array(val) if ARRAYS.include?(k) && !val.nil?
       sso.send("#{k}=", val)
     end
 
     decoded_hash.each do |k, v|
-      # 1234567
-      # custom.
-      #
-      if k[0..6] == "custom."
-        field = k[7..-1]
+      if field = k[/^custom\.(.+)$/, 1]
         sso.custom_fields[field] = v
       end
     end
 
     sso
+  end
+
+  def diagnostics
+    SingleSignOn::ACCESSORS.map {|a| "#{a}: #{send(a)}"}.join("\n")
   end
 
   def sso_secret
@@ -86,7 +102,7 @@ class SingleSignOn
   end
 
   def payload
-    payload = Base64.encode64(unsigned_payload)
+    payload = Base64.strict_encode64(unsigned_payload)
     "sso=#{CGI.escape(payload)}&sig=#{sign(payload)}"
   end
 
